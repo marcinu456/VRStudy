7f1b4e36ae92248d7d95fc169251f769f2c2c681
Marcin <marcinu456@gmail.com>
2021-11-08 10:39:01 +0100

    Button Actions And Splash Screens

--- LightPainter/Source/LightPainter/UI/PaintingPicker/PaintingGridCard.cpp
+++ LightPainter/Source/LightPainter/UI/PaintingPicker/PaintingGridCard.cpp
@@ -2,10 +2,20 @@
 
 
 #include "PaintingGridCard.h"
+#include "Kismet/StereoLayerFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
 
-void UPaintingGridCard::SetPaintingName(FString PaintingName)
+void UPaintingGridCard::SetPaintingName(FString _PaintingName)
 {
+	PaintingName = _PaintingName;
 
-	
 	SlotName->SetText(FText::FromString(PaintingName));
+
+	CardButton->OnClicked.AddDynamic(this, &UPaintingGridCard::CardButtonClicked);
+}
+
+void UPaintingGridCard::CardButtonClicked()
+{
+	UStereoLayerFunctionLibrary::ShowSplashScreen();
+	UGameplayStatics::OpenLevel(GetWorld(), "Canvas", true,"SlotName="+ PaintingName);
 }
